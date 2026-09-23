dc97b955fd33edefddf35615def5fbb0783f53ea
Joseph Buss <jmbuss2006@gmail.com>
2026-09-22 18:08:34 -0600

    Completed objectives 1 & 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What was your grade percent?");
+        string grade_input=Console.ReadLine();
+        float grade=float.Parse(grade_input);
+        if (grade>=90)
+        {
+            Console.WriteLine("Congrats, you got an A");
+            Console.WriteLine("You passed the class");
+        }
+
+        else if (grade<90&&grade>=80)
+        {
+            Console.WriteLine("Congrats, you got a B");
+            Console.WriteLine("You passed the class");
+        }
+
+        else if (grade<80&&grade>=70)
+        {
+            Console.WriteLine("Congrats, you got a C");
+            Console.WriteLine("You passed the class");
+        }
+
+        else if (grade<70&&grade>=60)
+        {
+            Console.WriteLine("Well, you got a D...");
+            Console.WriteLine("You didn't pass the class, but maybe next time you will.");
+        }
+
+        else 
+        {
+            Console.WriteLine("Well, you got a F...");
+            Console.WriteLine("You didn't pass the class, but maybe next time you will.");
+        }
     }
 }
